d577949647190233195b6b95a37a1164faf56b59
PaoloS1993 <61352138+PaoloS1993@users.noreply.github.com>
2025-03-22 17:50:26 +0100

    Update AuthButton.tsx image background

--- src/components/AuthButton.tsx
+++ src/components/AuthButton.tsx
@@ -18,7 +18,7 @@ export default function SignIn() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-cover bg-center px-4" 
     style={{
-      backgroundImage: `url('/farm-background.jpg')`,
+      backgroundImage: `url('/immagine_background_form.jpg')`,
       backgroundSize: 'cover',
       backgroundPosition: 'center',
       backgroundRepeat: 'no-repeat'
